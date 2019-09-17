5bd6cf0aa850ec49a92218ed43b75082d025fe72
andriwphilip <andriwphilip@gmail.com>
2019-09-17 19:57:00 -0300

    Pagination

--- ControlStock.Web/Controllers/CadastroController.cs
+++ ControlStock.Web/Controllers/CadastroController.cs
@@ -9,6 +9,8 @@ namespace ControleEstoque.Web.Controllers
 {
     public class CadastroController : Controller
     {
+        private const int _quantMaxLinhasPorPagina = 5;
+
         #region Usuários
 
         private const string _senhaPadrao = "{$127;$188}";
@@ -59,69 +61,6 @@ namespace ControleEstoque.Web.Controllers
                         model.Senha = "";
                     }
 
-                    var id = model.Salvar();
-                    if (id > 0)
-                    {
-                        idSalvo = id.ToString();
-                    }
-                    else
-                    {
-                        resultado = "ERRO";
-                    }
-                }
-                catch (Exception ex)
-                {
-                    resultado = "ERRO";
-                }
-            }
-
-            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
-        }
-
-        #endregion
-
-        #region Grupos de produtos
-
-        [Authorize]
-        public ActionResult GrupoProduto()
-        {
-            return View(GrupoProdutoModel.RecuperarLista());
-        }
-
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult RecuperarGrupoProduto(int id)
-        {
-            return Json(GrupoProdutoModel.RecuperarPeloId(id));
-        }
-
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult ExcluirGrupoProduto(int id)
-        {
-            return Json(GrupoProdutoModel.ExcluirPeloId(id));
-        }
-
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
-        {
-            var resultado = "OK";
-            var mensagens = new List<string>();
-            var idSalvo = string.Empty;
-
-            if (!ModelState.IsValid)
-            {
-                resultado = "AVISO";
-                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
-            }
-            else
-            {
-                try
-                {
                     var id = model.Salvar();
                     if (id > 0)
                     {
